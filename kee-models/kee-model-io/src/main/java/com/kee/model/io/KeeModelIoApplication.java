42a48ece9120175b4672c9fb1a287cb0c313faa4
zms <1974525360@qq.com>
2023-05-31 16:28:41 +0800

    微服务-新增io模块

---
+++ kee-models/kee-model-io/src/main/java/com/kee/model/io/KeeModelIoApplication.java
@@ -0,0 +1,34 @@
+package com.kee.model.io;
+
+
+
+import com.kee.common.security.annotation.EnableCustomConfig;
+import com.kee.common.security.annotation.EnableKeeFeignClients;
+import com.kee.common.swagger.annotation.EnableCustomSwagger2;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+
+/**
+ * @author Admin
+ */
+@EnableCustomConfig
+@EnableCustomSwagger2
+@EnableKeeFeignClients
+@EnableScheduling
+@SpringBootApplication
+public class KeeModelIoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KeeModelIoApplication.class,args);
+        System.out.println("启动成功"+"\n" +
+                " ________   ___      ___   ________      __   ___  _______   _______  \n" +
+                "(\"      \"\\ |\"  \\    /\"  | /\"       )    |/\"| /  \")/\"     \"| /\"     \"| \n" +
+                " \\___/   :) \\   \\  //   |(:   \\___/     (: |/   /(: ______)(: ______) \n" +
+                "   /  ___/  /\\\\  \\/.    | \\___  \\       |    __/  \\/    |   \\/    |   \n" +
+                "  //  \\__  |: \\.        |  __/  \\\\      (// _  \\  // ___)_  // ___)_  \n" +
+                " (:   / \"\\ |.  \\    /:  | /\" \\   :)     |: | \\  \\(:      \"|(:      \"| \n" +
+                "  \\_______)|___|\\__/|___|(_______/      (__|  \\__)\\_______) \\_______) \n" +
+                "                                                                      \n");
+    }
+}
